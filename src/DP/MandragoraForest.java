81a1b44284f9828cb7c32badc3f17b7f86fb90a4
Vladimir Orekhov <vladimir@Vladimirs-MacBook-Pro.local>
2016-11-05 12:14:28 +0100

    Mandragora Forest Hackerrank DP

---
+++ src/DP/MandragoraForest.java
@@ -0,0 +1,38 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * Mandragora forest
+ * https://www.hackerrank.com/challenges/mandragora
+ */
+public class MandragoraForest {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int k = 0; k < T; k++) {
+            int N = Integer.parseInt(br.readLine());
+            String[] vals = br.readLine().split(" ");
+            long[] vali = new long[N];
+            long sum = 0;
+            for (int i = 0; i < N; i++) {
+                vali[i] = Long.parseLong(vals[i]);
+                sum += vali[i];
+            }
+
+            Arrays.sort(vali);
+            long max_sum = 0;
+            for (int i = 0; i < N; i++) {
+                long max = Math.max((i + 1) * sum, (i + 2) * (sum - vali[i]));
+                if (max_sum < max) {
+                    max_sum = max;
+                }
+                sum -= vali[i];
+            }
+            System.out.println(max_sum);
+        }
+    }
+}

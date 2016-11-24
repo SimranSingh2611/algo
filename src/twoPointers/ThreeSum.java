5341657c6b3f9078abbc04b8340b0d88e84653a8
Vladimir Orekhov <vladimir.orekhov@booking.com>
2016-11-24 15:10:58 +0100

    3Sum two pointers solution

---
+++ src/twoPointers/ThreeSum.java
@@ -0,0 +1,50 @@
+package twoPointers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        int N = nums.length;
+        int i = 0;
+        while(i < N - 1) {
+            int begin = i + 1;
+            int end = N - 1;
+            
+            while(begin < end) {
+                int sum = nums[i] + nums[begin] + nums[end];
+                if(sum == 0) {
+                    List<Integer> triplet = new ArrayList<Integer>();
+                    triplet.add(nums[i]);
+                    triplet.add(nums[begin]);
+                    triplet.add(nums[end]);
+                    list.add(triplet);
+                    
+                    int num = nums[end];
+                    while(end > begin && nums[end] == num) {
+                        end--;
+                    }
+                } else if(sum < 0) {
+                    int num = nums[begin];
+                    while(end > begin && nums[begin] == num) {
+                        begin++;
+                    }
+                } else {
+                    int num = nums[end];
+                    while(end > begin && nums[end] == num) {
+                        end--;
+                    }
+                }
+            }
+            
+            int num = nums[i];
+            while(i < N && nums[i] == num) {
+                i++;
+            }
+        }
+        return list;
+    }
+}

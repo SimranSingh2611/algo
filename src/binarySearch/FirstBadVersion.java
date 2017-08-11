4c587153111ba043f5e7df1a02af256506837445
Vladimir Orekhov <vladimir.orekhov@booking.com>
2017-08-11 14:56:28 +0200

    Binary search warm-up level with comments

---
+++ src/binarySearch/FirstBadVersion.java
@@ -0,0 +1,31 @@
+package binarySearch;
+
+/**
+ * https://leetcode.com/problems/first-bad-version
+ * Binary search on predicate
+ */
+public class FirstBadVersion extends VersionControl {
+    public FirstBadVersion(boolean[] versions) {
+        super(versions);
+    }
+
+    public int firstBadVersion(int n) {
+        int begin = 1, end = n;
+        while(begin < end) {
+            int mid = begin + (int) Math.floor((end - begin)/2.0);
+            /**
+             * Sometimes you need control over which element will be chosen as mid in an array of two [x_i, x_i+1],
+             * in this case floor is not needed since 1/2 will give you 0 in Java.
+             * However, in some cases you might want to get ceil, in that case you want to be explicit.
+             * int mid = begin + (end - begin)/2; is not explicit enough
+             * Want to check how rounding affects binary search algorithm? try using ceil instead of floor in this example.
+              */
+            if(isBadVersion(mid)) {
+                end = mid;
+            } else {
+                begin = mid + 1;
+            }
+        }
+        return begin;
+    }
+}
---
+++ src/binarySearch/VersionControl.java
@@ -0,0 +1,26 @@
+package binarySearch;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class VersionControl {
+    private Set<Integer> badVersions;
+    private int totalVersions;
+
+    public VersionControl(boolean[] versions) {
+        totalVersions = versions.length;
+        badVersions = new HashSet<>();
+        for(int i = 0; i < totalVersions; i++) {
+            if(versions[i]) {
+                badVersions.add(i + 1);
+            }
+        }
+    }
+
+    public boolean isBadVersion(int version) {
+        if(version < 1 || version > totalVersions) {
+            throw new IndexOutOfBoundsException();
+        }
+        return badVersions.contains(version);
+    }
+}

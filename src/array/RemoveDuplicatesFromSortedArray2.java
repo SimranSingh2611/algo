a9ff23c0f2fbd1416de9de1fe57aa0e68419c2f8
Vladimir Orekhov <orekhov.volodya@gmail.com>
2018-03-25 12:28:12 +0200

    Add window string task

---
+++ src/array/RemoveDuplicatesFromSortedArray2.java
@@ -0,0 +1,24 @@
+package array;
+
+/*
+* Remove duplicates from a sorted array in-place
+* https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+ */
+public class RemoveDuplicatesFromSortedArray2 {
+    public int removeDuplicates(int[] nums) {
+        int insertionPos = 0;
+        int prev = 1;
+        int counter = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(counter == 0 || prev != nums[i]) {
+                counter = 0;
+                prev = nums[i];
+            }
+            if(counter < 2) {
+                nums[insertionPos++] = prev;
+                counter++;
+            }
+        }
+        return insertionPos;
+    }
+}
